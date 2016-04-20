9bfb217800efd0f54a3d6e4902919ba5f90972cb
kyleconkright <kyleconkright@gmail.com>
2016-04-19 18:35:06 -0700

    removed extra css folder

--- public/js/app.js
+++ public/js/app.js
@@ -4,6 +4,10 @@ app.controller('TweetController', ['$scope', '$http', function($scope, $http){
 	console.log("hello from tweet controller");
 
 	$http.get('/tweets').success(function(response){
+		console.log(response[0].user);
+		$scope.user = response[0].user.screen_name;
+		$scope.bio = response[0].user.description;
+		$scope.img_url = response[0].user.profile_image_url_https;
 		$scope.tweets = response;
 	})
 
