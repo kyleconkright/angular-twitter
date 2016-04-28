de1f20e4637aeee7a8632620d916eb6c0d5bf554
kyleconkright <kyleconkright@gmail.com>
2016-04-27 21:03:47 -0700

    added font-awesome

--- public/js/app.js
+++ public/js/app.js
@@ -2,28 +2,19 @@ var app = angular.module('app', []);
 
 app.controller('TweetController', ['$scope', '$http', function($scope, $http){
 
-
 	$http.get('/tweets').success(function(response){
 		$scope.user = response[0].user.screen_name;
 		$scope.bio = response[0].user.description;
 		$scope.img_url = 'https://twitter.com/'+$scope.user+'/profile_image?size=original';
 		$scope.tweets = response;
 		for (var i = 0 ; i < $scope.tweets.length ; i ++) {
-
-
-
 			if(Date.now() - Date.parse($scope.tweets[i].created_at) < 10000000) {
 	            $scope.tweets[i].date = moment.duration(Date.now() -  Date.parse($scope.tweets[i].created_at)).humanize() + ' ago';
-			// } else if(Date.now() - Date.parse($scope.tweets[i].created_at) < 1000000000) {
-			// 	$scope.tweets[i].date = moment($scope.tweets[i].created_at).startOf('day').fromNow();
 			} else {
 				$scope.tweets[i].date = moment($scope.tweets[i].created_at).calendar();
 			}
         }
-        console.log(Date.now() - Date.parse($scope.tweets[0].created_at));
-
-	})
-
+	});
 }])
 .directive('tweetCard', function() {
 	return {
